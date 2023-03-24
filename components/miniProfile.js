8d3e5eee654356b0a33852837e1d813ca9ece89c
Alizéa Massé <alizea777@hotmail.fr>
2023-03-24 14:57:05 +0100

    setting for deploy

--- components/miniProfile.js
+++ components/miniProfile.js
@@ -11,7 +11,7 @@ const MiniProfile = () => {
             <Image src={data?.user?.image} width="100" height="100" alt="photo de profil" className="w-16 h-16 rounded-full border p-[2px]"/>
             <div className="flex-1 mx-4">
                 <h2 className="font-bold">{data?.user.username}</h2>
-                <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
+                <h3 className="text-sm text-gray-400">Bienvenue sur Instagram</h3>
                 </div>
                 <button onClick={signOut} className="text-blue-400 text-sm font-semibold">Se déconnecter</button>   
                 </div>
